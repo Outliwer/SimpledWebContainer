bbfd61ccd52bbe1104a385e97012a440f368da14
Outliwer <595969125@qq.com>
2018-06-24 15:32:08 +0800

    完成了对HttpRequest初步的递归子程序编写，上传结果截图

--- src/main/java/Connector/HttpRequest.java
+++ src/main/java/Connector/HttpRequest.java
@@ -31,15 +31,17 @@ public class HttpRequest {
      * get the URI
      */
     public String getRequestURI() {
+        System.out.println("paramMap.size() " + paramMap.size());
         return paramMap.get("requestURI");
     }
 
+    /*
+     * the next token
+     */
     private void nextToken(){
         if (currentToken < tokens.length){
             currentToken ++;
             current = tokens[currentToken];
-            if (current == ' ' || current == '\t' || current == '\n')
-                nextToken();
         } else {
             // TODO: error message need
             System.out.println("the end of the tokens");
@@ -75,10 +77,10 @@ public class HttpRequest {
         if (current == 'E'){
             nextToken();
             if (current == 'T'){
-                paramMap.put("method","GET");
+                paramMap.put("METHOD","GET");
                 // TODO: current message need
                 System.out.println("the method : get");
-
+                parseURI();
             } else {
                 // TODO: error message need
                 System.out.println("the method is wrong");
@@ -91,6 +93,95 @@ public class HttpRequest {
 
 
     /*
+     * parse URI
+     */
+    private void parseURI(){
+        nextToken();
+        while (current == ' ' || current == '\t'){
+            nextToken();
+        }
+        if (current != '/'){
+            // TODO: error message need
+            System.out.println("there need a '/' , The URI is wrong");
+        } else {
+            StringBuilder sb = new StringBuilder();
+            nextToken();
+            while (current != '?' && current != ' '){
+                sb.append(current);
+                nextToken();
+            }
+            paramMap.put("requestURI", String.valueOf(sb));
+            // TODO: current message need
+            System.out.println("the requestURI :" + String.valueOf(sb));
+            if (current == ' '){
+                // There is no param in the request Param
+                // TODO: current message need
+                System.out.println("There is no param in the request Param");
+                parseHttpVersion();
+            } else {
+                // TODO: current message need
+                System.out.println("There is some params in the request Param");
+                parseParamUriRequest();
+                System.out.println("params have been recorded");
+                parseHttpVersion();
+            }
+        }
+    }
+
+    /*
+     * parse the param of the uriRequest
+     */
+    private void parseParamUriRequest(){
+        StringBuilder sb_key = new StringBuilder();
+        StringBuilder sb_value = new StringBuilder();
+        nextToken();
+        while (current != '='){
+            sb_key.append(current);
+            nextToken();
+        }
+        nextToken();
+        while (current != '&' && current != ' '){
+            sb_value.append(current);
+            nextToken();
+        }
+        uriParamMap.put(String.valueOf(sb_key),String.valueOf(sb_value));
+        // TODO: current message need
+        System.out.println(" params in the URI :  " +String.valueOf(sb_key)+ " : " +String.valueOf(sb_value));
+        if (current == '&'){
+            parseParamUriRequest();
+        }
+    }
+
+    /*
+     * parse the version of the Http
+     */
+    private void parseHttpVersion(){
+        nextToken();
+        while (current == ' ' || current == '\t'){
+            nextToken();
+        }
+        if (current == 'H'){
+            StringBuilder sb_key = new StringBuilder();
+            StringBuilder sb_value = new StringBuilder();
+            while (current != '/'){
+                sb_key.append(current);
+                nextToken();
+            }
+            nextToken();
+            while (current != '\n' && current != '\t' && current != ' '){
+                sb_value.append(current);
+                nextToken();
+            }
+            paramMap.put(String.valueOf(sb_key),String.valueOf(sb_value));
+            // TODO: current message need
+            System.out.println(" HTTP Version : " +String.valueOf(sb_key)+ " : " +String.valueOf(sb_value));
+            nextToken();
+            // TODO: 下一次分析开始  H
+//            System.out.println(current);
+        }
+    }
+
+    /*
      * change the inputStream to the CharArray
      */
     private char[] convertStreamToCharArray(InputStream is) {
@@ -99,7 +190,7 @@ public class HttpRequest {
         String line = null;
         try {
             while ((line = reader.readLine()) != null) {
-                sb.append(line + "/n");
+                sb.append(line + ' ');
             }
         } catch (IOException e) {
             e.printStackTrace();
